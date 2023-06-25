4f91d7f47554bbf9259af42f97652de6d6262b5b
Ayush Shrivastava <ayushshrivastava675@gmail.com>
2023-06-25 15:25:36 +0530

    new changes

---
+++ src/main/kotlin/basic/Datatypes.kt
@@ -0,0 +1,14 @@
+package basic
+
+fun main(args: Array<String>) {
+
+    var name:String = "Ayush"
+
+    var age:Int = 24
+    var isAlive:Boolean = true
+    var gender:Char = 'M'
+    var marks:Float = 48.8f
+    var percentage:Double = 61.7
+    var moreSmallNo:Byte = 8
+    var smallNo:Short = 8
+}
--- src/main/kotlin/basic/basic3.kt
+++ src/main/kotlin/basic/basic3.kt
@@ -4,4 +4,5 @@ fun main(args: Array<String>) {
 
     var person = Person1("Ayush Shrivastava")
     person.display()
+
 }
